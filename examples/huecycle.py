21124286869639df81a097206629dd19f6b285c5
ASW-Analyst <andy.wilson8@nhs.net>
2024-12-03 21:53:59 +0000

    Amend brightness and addition of timer for transition

--- examples/huecycle.py
+++ examples/huecycle.py
@@ -1,12 +1,15 @@
 from tree import RGBXmasTree
 from colorzero import Color, Hue
+import time
 
 tree = RGBXmasTree()
 
 tree.color = Color('red')
+tree.brightness = 0.05
 
 try:
     while True:
         tree.color += Hue(deg=1)
+        time.sleep(0.005)
 except KeyboardInterrupt:
     tree.close()
